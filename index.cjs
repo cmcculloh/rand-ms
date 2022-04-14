b08a2bf521975ecfcbfc3136fdf806c484c6d254
cmcculloh <cmcculloh@gmail.com>
2022-04-14 11:13:36 -0400

    linting errors

--- index.cjs
+++ index.cjs
@@ -1,49 +1,40 @@
 "use strict";
 
 const getMultiplier = interval => {
-	let multiplier;
 	switch (interval) {
 		case 'ms':
 		case 'milliseconds':
 		case 'millisecond':
-			multiplier = 1;
-			break;
+			return 1;
 		case 'm':
 		case 'minutes':
 		case 'minute':
-			multiplier = 60_000;
-			break;
+			return 60_000;
 		case 'h':
 		case 'hours':
 		case 'hour':
-			multiplier = 3_600_000;
-			break;
+			return 3_600_000;
 		case 'd':
 		case 'days':
 		case 'day':
-			multiplier = 86_400_000;
-			break;
+			return 86_400_000;
 		case 'w':
 		case 'weeks':
 		case 'week':
-			multiplier = 604_800_000;
-			break;
+			return 604_800_000;
 		case 'y':
 		case 'years':
 		case 'year':
-			multiplier = 31_557_600_000;
-			break;
+			return 31_557_600_000;
 		case 's':
 		case 'seconds':
 		case 'second':
 		default:
-			multiplier = 1000;
+			return 1000;
 	}
-
-	return multiplier;
 };
 
-const getMS = (p) => (p[1] * getMultiplier(p[2]));
+const getMS = p => (p[1] * getMultiplier(p[2]));
 
 const getRandomMS = (min, max) => {
 	const randMS = Math.floor((Math.random() * (max - min + 1)) + min);
@@ -53,14 +44,15 @@ const getRandomMS = (min, max) => {
 
 const regex = /([\d_]+)([a-z]*)/;
 
-const randomMS = (minString = "10s", maxString = "0s") => {
-	const min = regex.exec('' + minString);
+const randomMS = (minString = '10s', maxString = '0s') => {
+	const min = regex.exec(String(minString));
 	if (min === null) {
 		throw new TypeError(`Expected a number or string, eg: 1 or "1s", got "${minString}"`);
 	}
+
 	min[1] = Number(min[1]);
 
-	const max = regex.exec('' + maxString);
+	const max = regex.exec(String(maxString));
 	if (max === null) {
 		throw new TypeError(`Expected a number or string, eg: 1 or "1s", got "${maxString}"`);
 	}
